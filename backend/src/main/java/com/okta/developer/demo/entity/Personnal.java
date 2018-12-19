5d0f025f02b7e488b1fdcec8925f32fdcae8df5d
Chutivammm <vamp72994@gmail.com>
2018-12-19 13:57:54 +0700

    add class -close #18

---
+++ backend/src/main/java/com/okta/developer/demo/entity/Personnal.java
@@ -0,0 +1,38 @@
+package com.example.demo.Entity;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Personnal {
+
+    @Data
+    @Entity
+    @Getter
+    @Setter
+    @ToString
+    @EqualsAndHashCode
+
+    @Table(name = "Personnel")
+    public class Personnel {
+        @Id
+        @SequenceGenerator(name="Personnel_seq",sequenceName="Personnel_seq")
+        @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="Personnel_seq")
+        @Column(name="Personnel_ID",unique = true, nullable = true)
+        private   @NonNull Long id;
+        private   @NonNull String fname;
+        private   @NonNull String Lname;
+        private   @NonNull String nationality;
+        private   @NonNull String educational;
+        private   @NonNull String IdentificationNumber;
+        private   @NonNull String job_id;
+        private   @NonNull String phonenumber;
+        private   @NonNull String E_mail;
+        private   @NonNull String extraction;
+
+    }
