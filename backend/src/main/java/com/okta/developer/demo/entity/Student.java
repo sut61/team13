96ac7dbceb9a27634c3fac3745322eae4a594a8e
pangwara <waraphorn6456@gmail.com>
2018-12-20 23:11:41 +0700

    ”close

--- backend/src/main/java/com/okta/developer/demo/entity/Student.java
+++ backend/src/main/java/com/okta/developer/demo/entity/Student.java
@@ -1,30 +1,4 @@
-package entity;
-import javax.persistence.*;
-import lombok.*;
-public class Student {
-    @Id
-    private @NonNull
-    long id;
-    private @NonNull
-    String username;
-    private @NonNull
-    String num;
-    private @NonNull
-    String nation;
-    private @NonNull
-    String disease;
+packagre th.sut.sa.demo.Entity;
+public class Student{
 
-
-    public Student(String username,
-                   String num,
-                   String disease
-
-
-    ) {
-        this.username = username;
-        this.num = num;
-        this.disease = disease;
-
-
-    }
 }
