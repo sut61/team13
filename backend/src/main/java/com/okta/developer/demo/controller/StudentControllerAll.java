2b2f36a915e43bad19b90350e8911ee9427353ef
pangwara <45620486+pangwara@users.noreply.github.com>
2019-01-24 11:02:39 +0700

    Add files via upload

--- backend/src/main/java/com/okta/developer/demo/controller/StudentControllerAll.java
+++ backend/src/main/java/com/okta/developer/demo/controller/StudentControllerAll.java
@@ -1,72 +1,61 @@
-package com.okta.developer.demo.controller;
+package com.demo.Controller;
 
-
+import com.demo.entity.*;
+import com.demo.Repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import com.okta.developer.demo.entity.*;
-import com.okta.developer.demo.repository.*;
+
 import java.util.Collection;
-import java.util.Map;
-import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
-public class StudentControllerAll{
+public class StudentControllerAll {
 
-    @Autowired private GradeReporitory gradeReporitory;
-    @Autowired private GpaRepository gpaRepository;
-    @Autowired private MajorReporitory majorReporitory;
-    @Autowired private ProfessorReporitory professorReporitory;
-   @Autowired private StudentReporitory studentReporitory;
+    @Autowired private StudentReporitory studentRepository;
+    @Autowired private GenderReporitory genderRepository;
+    @Autowired private NationalityReporitory nationalityRepository;
+    @Autowired private ReligionRepository religionRepository;
 
-
-    @GetMapping("/Grades")
-    private Grade getGradeId(@PathVariable Long id){
-        return this.gradeReporitory.findById(id).get();
-    }
-
-
-
-    @GetMapping("/gpaa")
-    public Collection<Gpa> gpas() {
-        return gpaRepository.findAll().stream()
-                .collect(Collectors.toList());
-    }
-
-    @GetMapping("/Majors")
-    public Collection<Major> Major() {
-        return majorReporitory.findAll().stream()
-                .collect(Collectors.toList());
-    }
-    @GetMapping("/Professors")
-    public Collection<Professor> Professor() {
-        return professorReporitory.findAll().stream()
-                .collect(Collectors.toList());
-    }
-    @GetMapping(path = "/Students")
+    @GetMapping(path="/Student")
     public Collection<Student> Student() {
-        return studentReporitory.findAll().stream()
+        return studentRepository.findAll();}
+
+    @GetMapping(path="/Gender")
+    public List<Gender> Gender() {
+        return genderRepository.findAll().stream()
                 .collect(Collectors.toList());
     }
-    @PostMapping(path = "/grade/{point}/{gpa}/{major}/{pro}/{stu}")
-    public Grade grade(@PathVariable int point ,@PathVariable Long gpa , @PathVariable Long major , @PathVariable Long pro,
-                       @PathVariable Long stu){
-        Gpa g1 = gpaRepository.findById(gpa).get();
-        Major ma1 = majorReporitory.findById(major).get();
-        Professor pro1 = professorReporitory.findById(pro).get();
-        Student stu1 = studentReporitory.findById(stu).get();
-        Grade grade = new Grade(point,ma1,pro1,stu1,g1);
-        gradeReporitory.save(grade);
-        return grade;
+    @GetMapping(path="/Nationality")
+    public List<Nationality> Nationality() {
+        return nationalityRepository.findAll().stream()
+                .collect(Collectors.toList());
     }
+    @GetMapping(path="/Religion")
+    public List<Religion> Religion() {
+        return religionRepository.findAll().stream()
+                .collect(Collectors.toList());
+    }
+    @PostMapping("/student/{ge_id}/{nationality_id}/{religion_id}/{username}/{num}/{nation}/{disease}/{allergy}")
+    public Student student(@PathVariable long ge_id,@PathVariable long nationality_id,@PathVariable long religion_id
+            ,@PathVariable String username,@PathVariable String disease,@PathVariable String allergy,@PathVariable String num,@PathVariable String nation){
 
+        Gender gender = genderRepository.findById(ge_id).get();
+        Nationality nationality = nationalityRepository.findById(nationality_id).get();
+        Religion religion = religionRepository.findById(religion_id).get();
+
+        Student student = new Student();
+        student.setUsername(username);
+        student.setNum(num);
+        student.setGender(gender);
+        student.setNationality(nationality);
+        student.setReligion(religion);
+        student.setNation(nation);
+        student.setDisease(disease);
+        student.setAllergy(allergy);
+        studentRepository.save(student);
+        return student;
+    }
 }
+
